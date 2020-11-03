6c2533f95211cb5f839292b4c6b42b3ac4eb6d1a
unknown <taylorvought@gmail.com>
2020-11-03 08:15:41 -0800

    tues work

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY2hpY2hvMTIzIiwiYSI6ImNrZ3FxMzE3bDB0MHYycXFpdHNhNnVmYjEifQ.WV53FHDNDQTxueQqEWPIbw";
---
+++ static/js/logic.js
@@ -0,0 +1,72 @@
+var myMap = L.map("map", {
+    center: [40.7, -73.95],
+    zoom: 3
+  });
+  
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+  var url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
+  function mag_scale(feat){
+    if(feat.properties.mag<3){
+        return 100;
+      }
+    else if(feat.properties.mag>=3 && feat.properties.mag<=3.9 ){
+        return 200;
+      }
+    else if(feat.properties.mag>=4 && feat.properties.mag<=4.9){
+        return 300;
+      }
+    else if(feat.properties.mag>=5 && feat.properties.mag<=5.9){
+        return 400;
+        }
+    else if(feat.properties.mag>=6 && feat.properties.mag<=6.9){
+            return 500;
+        }
+    else if(feat.properties.mag>=7 && feat.properties.mag<=7.9){
+        return 600;
+        }
+    else {
+        return 700;
+    }
+  }
+  function depth_scale(feat){
+      if (feat.geometry.coordinates['2']<10){
+          return 'Green';
+      }
+      else if (feat.geometry.coordinates['2']>=10 && feat.geometry.coordinates['2']<30){
+        return 'LawnGreen';
+    }
+    else if (feat.geometry.coordinates['2']>=30 && feat.geometry.coordinates['2']<50){
+        return 'GreenYellow';
+    }
+    else if (feat.geometry.coordinates['2']>=50 && feat.geometry.coordinates['2']<70){
+        return '#F0E68C';
+    }
+    else if (feat.geometry.coordinates['2']>=70 && feat.geometry.coordinates['2']<90){
+        return 'Yellow';
+    }
+    else {
+        return 'GoldenRod';
+    }
+  }
+
+  d3.json(url,function(resp){
+      console.log(resp.features['25'].geometry.coordinates['0'])
+      console.log(depth_scale(resp.features['50']))
+      resp.features.forEach(function(data){
+        L.circle([data.geometry.coordinates['0'], data.geometry.coordinates['1']],{
+            color: 'white',
+            fillColor: depth_scale(data),
+            fillOpacity: .8,
+            radius: mag_scale(data)*200
+        }).bindPopup('<h2> Magnitude: '+data.properties.mag+'</h2>'+'<hr> <h3> Depth:'+data.geometry.coordinates['2']+'</h3>').addTo(myMap)
+      })
+  })
